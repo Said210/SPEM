e9704a51e553f14c06aebabcbac1f1e95c97814d
Said Pelaez <said@cloro.mx>
2015-12-21 11:01:12 -0600

    ...

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Inicio | SPEM</title>
+	<link rel="stylesheet" href="assets/css/app.css">
+</head>
+<body>
+	<div class="container">
+		<div class="child">
+     <!--Menu-->
+			 <section class="full top menu">
+               <div class="left">
+                 <img src="assets/images/logo.png" class="logo" alt="">
+               </div>
+			   <div class="right">
+			     <ul class="">
+			       <li class="menu-item">Nosotros</li>
+			       <li class="menu-item">¿Para quienes?</li>
+			       <li class="menu-item">¿Qué hacemos?</li>
+			       <li class="menu-item">Consultenos</li>
+			       <li class="menu-item">¿Dónde?</li>
+			     </ul>
+			   </div>
+			 </section>
+		<!--End-->
+		<!--About-->
+            <section class="us fix">
+              <div class="half white">
+                <img src="assets/images/logo-color.png" width="100%" class="hidden" alt="">
+                <div class="content">
+                  <img src="assets/images/logo-color.png" class="logo" alt="">
+                </div>
+              </div>
+              <div class="half">
+                <img src="assets/images/logo-color.png" width="100%" class="hidden" alt="">
+                <div class="content med su_bo">Servicios de consultoría especializada en temas legales y administrativos, para personas, empresas y gobiernos.</div>
+              </div>
+              <div class="us">
+                <div class="block padding_image" id="us_1"></div>
+                <div class="block padding_image" id="us_2"></div>
+                <div class="block padding_image" id="us_3"></div>
+                <div class="block" id="us_content">
+                  <h2>¿QUIENES SOMOS?</h2>
+                  <hr>
+                  Un equipo multidisciplinario enfocado en desarrollar soluciones para nuestros clientes.
+                </div>
+              </div>
+            </section>
+		<!--End-->
+		<!--Who-->
+            <section class="who fix">
+              <div class="block padding_image" id="who_1"></div>
+              <div class="block" id="who_2">
+               
+                <div class="full" id="who_content">
+                  <h2>¿PARA QUIENES?</h2>
+                  <hr class="purple">
+                  Nuestros clientes son empresarios, pequeños comerciantes, emprendedores, funcionarios públicos y privados, profesionistas, amas de casa…    
+                </div>
+              </div>
+              <div class="block white" id="who_3">
+                <div class="content white">
+                  “SPEM es una consultoría que se enfoca en desarrollar soluciones efectivas para sus clientes”
+                </div>
+              </div>
+              <div class="block" id="who_4">
+                <div class="full" id="who_content">
+                  <span class="big">A su lado desde</span><br><br>
+                  <span class="super_big su-bo">2006</span><br><br>
+                  <div class="big">México</div>
+                </div>
+              </div>
+            </section>
+		<!--End-->
+        <!--what-->
+		    <section class="what fix">
+		      <div class="block" id="what_1">
+		        <div class="full" id="what_content">
+                  <h2>¿Qué hacemos?</h2>
+                  <hr class="purple">
+                  Le atendemos para conocer sus problemas y necesidades, y en función de ellas poder diseñar y construir las mejores soluciones.              
+                </div>
+		      </div>
+		      <div class="block" id="what_2">
+		        <div class="full" id="what_content">
+		          <div class="med">Su problema se relaciona con...</div>
+		          <div class="little-button">Elemento 1</div>
+		          <div class="little-button">Elemento 2</div>
+		          <div class="little-button">Elemento 3</div>
+		          <div class="little-button">Elemento 4</div>
+		          <div class="little-button">Elemento 5</div>
+		        </div>
+		      </div>
+		      <div class="block" id="what_3">
+		        <div class="full" id="what_content"><br><br>
+		          <div class="little-button">Elemento 1</div>
+		          <div class="little-button">Elemento 2</div>
+		          <div class="little-button">Elemento 3</div>
+		          <div class="little-button">Elemento 4</div>
+		        </div>
+		      </div>
+		      <div class="block padding_image" id="what_4"></div>
+		    </section>
+		<!--End-->
+		</div>
+	</div>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,6 @@
+#To Work on the project
+Compile SCSS to CSS: sass --watch dev/scss:assets/css
+Compile TS to JS:    tsc -w --outFile assets/js/app.js dev/ts/all --module commonjs
+
+
+Cloro MX
