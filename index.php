ab0877586f85562ef473574252064a08f26aad72
Said Pelaez <said@cloro.mx>
2015-12-22 08:05:48 -0600

    ...

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
               </div>
               <div class="half">
                 <img src="assets/images/logo-color.png" width="100%" class="hidden" alt="">
-                <div class="content med su_bo">Servicios de consultoría especializada en temas legales y administrativos, para personas, empresas y gobiernos.</div>
+                <div class="content med">Servicios de consultoría especializada en temas legales y administrativos, para personas, empresas y gobiernos.</div>
               </div>
               <div class="us">
                 <div class="block padding_image" id="us_1"></div>
@@ -101,8 +101,69 @@
 		        </div>
 		      </div>
 		      <div class="block padding_image" id="what_4"></div>
+		      <div class="what fix">
+                <div class="block padding_image" id="what_5"></div>
+                <div class="three_quarters" id="what_6">
+                <img src="http://www.kissuomo.it/wp-content/uploads/2013/12/tumblr_mps6biKdXe1s4yg05o1_r1_500.jpg" width="27.1%" class="hidden" alt="">
+                <div class="content med">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore repudiandae eligendi neque inventore, culpa quasi atque deleniti eius, fugit quas consequuntur animi incidunt nemo, laboriosam error commodi, quaerat nihil suscipit.</div></div>
+              </div>
+		    </section>
+		    <section class="space fix">
+		      <div class="block padding_image" id="consult_1"></div>
+              <div class="block padding_image" id="consult_2"></div>
+              <div class="block padding_image" id="consult_3"></div>
+              <div class="block padding_image" id="consult_4"></div>
 		    </section>
 		<!--End-->
+		<!--Consult-->
+		    <section class="consult fix">
+		      <div class="full">
+		        <p class="super_big su_bo" align="center">CONSULTENOS:</p>
+		        <p id="form_consult" align="center" class="li big">
+		          Soy <select name="iam" id="iam">
+		            <option value="fisica">persona física</option>
+		            <option value="moral">persona moral</option>
+		          </select>
+		          y solicito
+		          <select name="service" id="service">
+                    <option value="cita">Cita</option>
+                    <option value="pregunta">Hacer una pregunta</option>
+                    <option value="planteaminto">Hacer un planteamiento</option>
+                  </select>
+                  
+		        </p>
+		      </div>
+		    </section>
+		<!--End-->
+		<!--Where-->
+		    <section class="where us fix" id="where">
+              <div class="box">
+                <span class="super_big su_bo">¿Dónde?</span>
+                <hr>
+                <p class="med">
+                  Barranca del Muerto 525, Pisos 8 y 9,<br>
+                  Colonia Merced Gómez <br>
+                  11600.
+                </p>
+              </div>
+		      <div class="full">
+		        <iframe width="100%" height="500px" frameborder="0" style="border:0"
+src="https://www.google.com/maps/embed/v1/place?q=Baranca%20del%20muerto%20525&key=AIzaSyCy79BLrLPX0oBnwx09f6ap6-ugs9zmgZQ" allowfullscreen></iframe>
+		      </div>
+		    </section>
+		<!--End-->
+		    <section class="footer fix">
+		      <div class="full" align="center">
+		        Barranca del Muerto 525, Pisos 8 y 9, <br>
+                Colonia Merced Gómez <br>
+                11600  México, D.F. <br><br>
+
+                Tels. 1206 5090  /  1206  5091 <br>
+                1206 5092 <br><br>
+
+                <a href="mailto:contacto@spem.com.mx" target="_blank">contacto@spem.com.mx </a><br>
+		      </div>
+		    </section>
 		</div>
 	</div>
 </body>
